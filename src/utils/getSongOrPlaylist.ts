dfec6d9d97d6e018df27072a794e50f401d529d3
ductridev <trihd123@gmail.com>
2025-06-23 18:42:29 +0700

    feat: get or search song

---
+++ src/utils/getSongOrPlaylist.ts
@@ -0,0 +1,23 @@
+import DisTube, { ExtractorPlugin, InfoExtractorPlugin, PlayableExtractorPlugin, Playlist, Song } from "distube";
+import { getPluginForUrl } from "./getPluginNameForUrl";
+
+export const getSongOrPlaylist = async (distube: DisTube, query: string) => {
+    const plugin = await getPluginForUrl(distube, query);
+    if (plugin instanceof ExtractorPlugin && !(await plugin.validate(query))) {
+        return await plugin.searchSong(query, {});
+    } else if (plugin.constructor.name === 'YtDlpPlugin' && plugin instanceof PlayableExtractorPlugin) {
+        // Get ExtractorPlugin & search song
+        let searchPlugin: ExtractorPlugin | undefined = await plugin.distube.plugins.find(plugin => plugin instanceof ExtractorPlugin);
+        if (!searchPlugin) throw Error("Please add atleast 1 ExtractorPlugin.");
+
+        let song = await searchPlugin.searchSong(query, {});
+
+        if (song && song.url) {
+            return plugin.resolve(song.url, {});
+        } else {
+            return null;
+        }
+    } else {
+        return await plugin.resolve(query, {});
+    }
+}
