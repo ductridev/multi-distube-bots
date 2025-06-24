40c4853656bcea5a8f597f6836d5d68796b25fd0
ductridev <trihd123@gmail.com>
2025-06-24 14:04:59 +0700

    feat: change volume

---
+++ src/commands/music/volume.ts
@@ -0,0 +1,41 @@
+// src/commands/music/volume.ts
+/* 
+    Command: volume
+    Description: Changes the volume.
+    Usage: b!volume <0-200>
+    Category: music
+    Aliases: v
+*/
+
+import { Command } from "../../@types/command";
+import { replyWithEmbed } from "../../utils/embedHelper";
+
+export const volume: Command = {
+    name: 'volume',
+    description: 'Thay đổi âm lượng.',
+    usage: 'b!volume <0-200>',
+    category: 'music',
+    aliases: ['v'],
+    execute: async (message, args, distube) => {
+        try {
+            const vc = message.member?.voice.channel;
+            if (!vc) {
+                await replyWithEmbed(message, 'error', 'Bạn cần vào kênh thoại.');
+                return;
+            }
+
+            const volume = parseFloat(args[0]);
+
+            if (isNaN(volume) || volume < 0 || volume > 200) {
+                await replyWithEmbed(message, 'error', 'Âm lượng phải nằm trong khoảng 0 đến 200.');
+                return;
+            }
+
+            distube.getQueue(message)?.setVolume(volume);
+            await replyWithEmbed(message, 'success', `Đã thay đổi âm lượng phát thành ${volume}.`);
+        } catch (err) {
+            console.error(err);
+            // Do nothing
+        }
+    },
+}
