686a8c948e84bd4b4134cf271539ae6d395c3f25
ductridev <trihd123@gmail.com>
2025-06-26 20:23:45 +0700

    Add to Queue & Play feat: check vc permissions feat: add to queue & play

--- src/commands/music/playrecent.ts
+++ src/commands/music/playrecent.ts
@@ -9,16 +9,15 @@
 
 import { Command } from '../../@types/command';
 import {
-    EmbedBuilder,
     GuildTextBasedChannel,
     Message,
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
 } from 'discord.js';
 import { RecentTrackModel } from '../../models/RecentTrack';
 import { replyWithEmbed } from '../../utils/embedHelper';
 import { setInitiator } from '../../utils/sessionStore';
+import { getSongOrPlaylist } from '../../utils/getSongOrPlaylist';
+import { Playlist, Song } from 'distube';
+import { canBotJoinVC } from '../../utils/voicePermission';
 
 const playrecent: Command = {
     name: 'playrecent',
@@ -34,7 +33,13 @@ const playrecent: Command = {
                 return;
             }
 
-            setInitiator(message.guildId!, message.author.id);
+            const error = canBotJoinVC(vc, message.client.user!.id);
+            if (error) {
+                await replyWithEmbed(message, 'error', error);
+                return;
+            }
+
+            setInitiator(message.guildId!, vc.id, message.author.id);
 
             try {
                 const recent = await RecentTrackModel.findOne({ userId: message.author.id });
@@ -44,10 +49,35 @@ const playrecent: Command = {
                     return;
                 }
 
-                await distube.play(vc, recent.tracks[0], {
-                    member: message.member!,
-                    textChannel: message.channel as GuildTextBasedChannel,
-                });
+                const songOrPlaylist = await getSongOrPlaylist(distube, recent.tracks[0]);
+
+                if (songOrPlaylist instanceof Playlist && songOrPlaylist.songs.length === 0) {
+                    await replyWithEmbed(message, 'error', 'Không thể phát playlist này.');
+                    return;
+                } else if (songOrPlaylist instanceof Song && songOrPlaylist.duration === 0) {
+                    await replyWithEmbed(message, 'error', 'Không thể phát bài hát này.');
+                    return;
+                } else if (!songOrPlaylist) {
+                    await replyWithEmbed(message, 'error', 'Không tìm thấy bài hát nào phù hợp.');
+                    return;
+                }
+
+                if (!songOrPlaylist.url) {
+                    await replyWithEmbed(message, 'error', 'Không thể phát bài hát hoặc playlist này.');
+                    return;
+                }
+
+                let queue = distube.getQueue(message);
+
+                if (!queue) {
+                    queue = await distube.queues.create(vc, message.channel as GuildTextBasedChannel)
+                }
+
+                queue.addToQueue(songOrPlaylist instanceof Playlist ? songOrPlaylist.songs : songOrPlaylist);
+
+                if (!queue.playing) {
+                    queue.play();
+                }
 
                 await replyWithEmbed(message, 'success', '▶️ Đã phát lại bài gần nhất bạn đã nghe.');
             } catch (err) {
