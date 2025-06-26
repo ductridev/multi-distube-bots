1f6d1445cffa5e0f53c701deb20bb48b1208e365
ductridev <trihd123@gmail.com>
2025-06-26 20:12:38 +0700

    Distube Events: feat: move cancel no song timeout to play song. feat: display error if cannot play.

--- src/bot/distubeEvents.ts
+++ src/bot/distubeEvents.ts
@@ -3,10 +3,10 @@ import { DisTube, Events, Queue, Song } from 'distube';
 import ExtendedClient from '../@types/extendedClient';
 import { sendWithEmbed } from '../utils/embedHelper';
 import { GuildTextBasedChannel } from 'discord.js';
-import { cancelNoSongTimeout } from '../utils/botDisconnectTimeout';
 import { onFinishQueue } from '../events/distube/onFinishQueue';
 import { onFinishSong } from '../events/distube/onFinishSong';
 import { onPlaySong } from '../events/distube/onPlaySong';
+import { onNoRelated } from '../events/distube/onNoRelated';
 
 export function registerDisTubeEvents(
     distube: DisTube,
@@ -19,14 +19,18 @@ export function registerDisTubeEvents(
     // distube.on(Events.DEBUG, console.debug);
     distube.on(Events.FINISH, (queue: Queue) => onFinishQueue(queue, noSongTimeouts));
 
-    distube.on(Events.ADD_SONG, (queue: Queue) => cancelNoSongTimeout(queue, noSongTimeouts));
-    distube.on(Events.ADD_LIST, (queue: Queue) => cancelNoSongTimeout(queue, noSongTimeouts));
-
     distube.on(Events.FINISH_SONG, (queue: Queue, song: Song) => onFinishSong(queue, song, noListenerTimeouts));
-    distube.on(Events.PLAY_SONG, (queue: Queue, song: Song) => onPlaySong(queue, song));
+    distube.on(Events.PLAY_SONG, (queue: Queue, song: Song) => onPlaySong(queue, song, noSongTimeouts));
+
+    distube.on(Events.NO_RELATED, (queue: Queue) => onNoRelated(queue, noSongTimeouts));
 
     distube.on(Events.ERROR, (error: Error, queue: Queue, song: Song | undefined) => {
         console.error(`[${name}] Error in distube:`, error);
+        if (song) {
+            console.error(`[${name}] Song:`, song.name, song.stream.playFromSource ? song.stream.url : song.url);
+            sendWithEmbed(queue.textChannel as GuildTextBasedChannel, 'error', `Không thể phát bài hát: [${song.name}](${song.url}).`);
+            return;
+        }
         sendWithEmbed(queue.textChannel as GuildTextBasedChannel, 'error', 'Có gì đó đã xảy ra khi chúng tôi đang phát nhạc.');
     });
 }
