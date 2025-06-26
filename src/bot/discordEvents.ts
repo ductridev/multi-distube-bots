e4135c660569bc15d45aaaa7c39993fa13820556
ductridev <trihd123@gmail.com>
2025-06-26 20:11:25 +0700

    feat: handle slash command

--- src/bot/discordEvents.ts
+++ src/bot/discordEvents.ts
@@ -1,10 +1,11 @@
 // src/bot/discordEvents.ts
-import { Message, VoiceState } from 'discord.js';
+import { Interaction, Message, VoiceState } from 'discord.js';
 import { DisTube } from 'distube';
 import ExtendedClient from '../@types/extendedClient';
 import BotInstance from '../@types/botInstance';
 import { onVoiceStateUpdate } from '../events/discord/onVoiceStateUpdate';
 import { onMessageCreate } from '../events/discord/onMessageCreate';
+import { onInteractionCreate } from '../events/discord/onInteractionCreate';
 
 export function registerDiscordEvents(
     client: ExtendedClient,
@@ -16,6 +17,6 @@ export function registerDiscordEvents(
     activeBots: BotInstance[],
 ) {
     client.on('voiceStateUpdate', (oldState: VoiceState, newState: VoiceState) => onVoiceStateUpdate(oldState, newState, activeBots, noListenerTimeouts, distube));
-
+    client.on('interactionCreate', (interaction: Interaction) => onInteractionCreate(interaction, activeBots, mainPrefix, client, distube, name));
     client.on('messageCreate', async (message: Message) => onMessageCreate(message, activeBots, mainPrefix, client, distube, name));
 }
